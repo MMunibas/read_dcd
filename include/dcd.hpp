c0e22dae164d81b9e96bf817cda501b5cdee4522
FHedin <hedin.florent@gmail.com>
2013-03-27 11:21:20 +0100

    license info for cpp and hpp files

--- include/dcd.hpp
+++ include/dcd.hpp
@@ -1,9 +1,20 @@
-/* 
- * File:   dcd.hpp
- * Author: hedin
- *
- * Created on March 27, 2013, 9:55 AM
- */
+/*
+    read_dcd : c++ class + main file example for reading a CHARMM dcd file
+    Copyright (C) 2013  Florent Hedin
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef DCD_HPP_INCLUDED
 #define DCD_HPP_INCLUDED
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,19 @@
-/* 
- * File:   main.cpp
- * Author: hedin
- *
- * Created on March 27, 2013, 9:55 AM
+/*
+ *  read_dcd : c++ class + main file example for reading a CHARMM dcd file
+ *  Copyright (C) 2013  Florent Hedin
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <cstdlib>
@@ -12,9 +23,6 @@
 
 using namespace std;
 
-/*
- * 
- */
 int main() {
 
     // instance of a new object DCD attached to a dcd file
